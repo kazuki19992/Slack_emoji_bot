40a21fbba000a082898b3d73212b145670c44047
kazuki19992 <kushida98@gmail.com>
2020-09-14 19:48:28 +0900

    command変数を追加

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ $body = file_get_contents('php://input');
 $json = json_decode($body, true);
 
 // slackのWebhookのURLを読み込む
-$slack_url = file_get_contents('./url.txt');
-define('WEBHOOK_URL', $slack_url);
+$webhook_url = file_get_contents('./url.txt');
 
 header('Content-type: text/plain; charset=utf-8');
 
@@ -21,16 +20,19 @@ if($json['type']==="url_verification"){
             $emoji = $json['event']['name'];
             $msg = "新しい絵文字`:{$emoji}:`が追加されたよ！\n:{$emoji}:\n";
             $msg_json = json_encode(['text' => $msg]);
+            $command = "curl -X POST -H 'Content-type: application/json' --data '". $msg_json. " '". $webhook_url ."'";
 
             // 送信コマンド実行
-            exec("curl -X POST -H 'Content-type: application/json' --data '". $msg_json. " '". WEBHOOK_URL ."'");
+            exec($command);
             exit;
         }else{
             // エイリアス追加
             $msg = "新しいエイリアスが追加されたよ！\n";
             $msg_json = json_encode(['text' => $msg]);
-            exec("curl -X POST -H 'Content-type: application/json' --data '". $msg_json. " '". WEBHOOK_URL ."'");
+            $command = "curl -X POST -H 'Content-type: application/json' --data '". $msg_json. " '". $webhook_url ."'";
+            exec($command);
             exit;
         }
     }
 }
+
